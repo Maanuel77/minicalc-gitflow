8c255e79356b69373d5bb404220bf92a78af9b53
Manuel Martinez <martinez.maman25@triana.salesianos.edu>
2026-01-15 10:18:23 +0100

    chore: initial version.

---
+++ src/minicalc/MiniCalc.java
@@ -0,0 +1,6 @@
+package minicalc;
+
+public class MiniCalc {
+    public static int add(int a, int b) { return a + b; }
+    // TODO: implement subtract
+}
---
+++ src/module-info.java
@@ -0,0 +1,8 @@
+/**
+ * 
+ */
+/**
+ * 
+ */
+module Minicalc_gitflow {
+}
